38a63518f3a43b02533faefe8496a13a11ef2bde
zhongyi51 <zhongyi51@users.noreply.github.com>
2026-01-03 21:18:39 +0800

    feat:update to 2.0

---
+++ src/repr_storage.rs
@@ -0,0 +1,75 @@
+use std::{
+    ptr::NonNull,
+    sync::atomic::{AtomicPtr, Ordering},
+};
+
+mod private {
+    pub trait Sealed {}
+}
+
+/// A marker trait for atomic pointer storage.
+pub trait AtomicPointerStorage: PointerStorage {
+    fn compare_exchange(
+        &self,
+        current: NonNull<()>,
+        new: NonNull<()>,
+    ) -> Result<NonNull<()>, NonNull<()>>;
+}
+
+/// A storage trait for flagged pointers' `repr` field.
+pub trait PointerStorage: Sized + private::Sealed {
+    fn new(ptr: NonNull<()>) -> Self;
+    fn load(&self) -> NonNull<()>;
+    fn set(&mut self, ptr: NonNull<()>) -> NonNull<()>;
+}
+
+impl private::Sealed for NonNull<()> {}
+
+impl PointerStorage for NonNull<()> {
+    fn new(ptr: NonNull<()>) -> Self {
+        ptr
+    }
+    fn load(&self) -> NonNull<()> {
+        *self
+    }
+    fn set(&mut self, ptr: NonNull<()>) -> NonNull<()> {
+        let old = *self;
+        *self = ptr;
+        old
+    }
+}
+
+impl private::Sealed for AtomicPtr<()> {}
+
+impl PointerStorage for AtomicPtr<()> {
+    fn new(ptr: NonNull<()>) -> Self {
+        Self::new(ptr.as_ptr())
+    }
+    fn load(&self) -> NonNull<()> {
+        unsafe { NonNull::new_unchecked(self.load(Ordering::Acquire)) }
+    }
+    fn set(&mut self, ptr: NonNull<()>) -> NonNull<()> {
+        let old = self.swap(ptr.as_ptr(), Ordering::Release);
+        unsafe { NonNull::new_unchecked(old) }
+    }
+}
+
+impl AtomicPointerStorage for AtomicPtr<()> {
+    fn compare_exchange(
+        &self,
+        current: NonNull<()>,
+        new: NonNull<()>,
+    ) -> Result<NonNull<()>, NonNull<()>> {
+        let res = self.compare_exchange(
+            current.as_ptr(),
+            new.as_ptr(),
+            Ordering::AcqRel,
+            Ordering::Acquire,
+        );
+
+        match res {
+            Ok(ptr) => unsafe { Ok(NonNull::new_unchecked(ptr)) },
+            Err(ptr) => unsafe { Err(NonNull::new_unchecked(ptr)) },
+        }
+    }
+}
